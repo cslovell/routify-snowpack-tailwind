790700117bff187eb4b3e05b409875e2e70e791e
Sean Lovell <lovells@un.org>
2021-02-13 06:13:09 -0500

    Fixed Flash of unstyled content issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,11 @@
 <!-- App.svelte -->
 <script>
-  import './main.css';
   import { Router } from '@roxi/routify';
   import { routes } from "./.routify/routes";
 </script>
+
+<svelte:head>
+	<link rel="stylesheet" href="./dist/main.css">
+</svelte:head>
 
 <Router {routes} />
--- src/pages/_layout.svelte
+++ src/pages/_layout.svelte
@@ -1,1 +1,15 @@
+<style>
+    .container {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+        grid-template-rows: 1fr 1fr;
+    }
+</style>
+
+<div class="container">
+    <div class="bg-red-200"></div>
+    <div class="bg-blue-200"></div>
+
+</div>
+<h1>Layout Page</h1>
 <slot></slot>
